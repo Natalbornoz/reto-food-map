36f1dd8240b120328c010f16fbb835b07acd525a
= <natalbornoz@gmail.com>
2017-12-14 17:04:20 -0300

    interactividad

---
+++ assets/js/app.js
@@ -0,0 +1,9 @@
+// $(function(){
+//    setTimeout(function() {
+//       $('#splash').fadeOut(500);
+//    }, 2000);
+// });
+
+// $( "#target" ).click(function() {
+//   alert( "Handler for .click() called." );
+// });
